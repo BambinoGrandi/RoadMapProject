b3d88c4e057d9cb583a3811b97d797c40ca7aab6
ryaboshapkodi <ryaboshapkodi@sovcombank.ru>
2022-12-25 18:21:19 +0300

    Initial commit

---
+++ app/src/main/java/ru/grandibambino/roadmapproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.grandibambino.roadmapproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.grandibambino.roadmapproject.ui.theme.RoadMapProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
